c473ee3498e606bb1c2a8b22284b91f04444946b
MrSayyod <ssayyod@gmail.com>
2020-02-21 03:12:52 +0500

    config was changed into never-multi-line

--- stylelint.config.js
+++ stylelint.config.js
@@ -82,7 +82,7 @@ module.exports = {
     "number-leading-zero": "always",
     "number-no-trailing-zeros": true,
     "property-case": "lower",
-    "rule-empty-line-before": [ "always-multi-line", {
+    "rule-empty-line-before": [ "never-multi-line", {
       except: ["first-nested"],
       ignore: ["after-comment"],
     } ],
